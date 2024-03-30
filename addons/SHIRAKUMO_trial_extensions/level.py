13d9cfcca9a43f13344b2d820a4e8c058e61f39d
Shinmera <shinmera@tymoon.eu>
2024-03-30 13:01:42 +0100

    Rebake ui

--- addons/SHIRAKUMO_trial_extensions/level.py
+++ addons/SHIRAKUMO_trial_extensions/level.py
@@ -29,12 +29,61 @@ def rebake_all():
                                    not context.object.khr_physics_extra_props.is_trigger):
             rebake(obj)
 
-def rebake_and_export():
-    rebake_all()
-    bpy.ops.export_scene.gltf(check_existing=False)
+class ReBake(bpy.types.Operator):
+    bl_idname = "shirakumo_trial.rebake"
+    bl_label = "ReBake Selected"
+    
+    @classmethod
+    def poll(cls, context):
+        for obj in context.selected_objects:
+            if obj.type == 'MESH':
+                return True
+        return None
+    
+    def execute(self, context):
+        for obj in context.selected_objects:
+            rebake(obj)
+
+class ReBakeAll(bpy.types.Operator):
+    bl_idname = "shirakumo_trial.rebake_all"
+    bl_label = "ReBake Selected"
+    def execute(self, context):
+        rebake_all()
+
+class ReBakeAndExport(bpy.types.Operator):
+    bl_idname = "shirakumo_trial.rebake_and_export"
+    bl_label = "ReBake Selected"
+    def execute(self, context):
+        rebake_all()
+        bpy.ops.export_scene.gltf(check_existing=False)
+
+class SHIRAKUMO_PT_bake_panel(bpy.types.Panel):
+    bl_idname = "SHIRAKUMO_PT_bake_panel"
+    bl_label = "Rebake"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Edit"
+    bl_context = "objectmode"
+
+    def draw(self, context):
+        obj = context.object
+        layout = self.layout
+        flow = layout.grid_flow(row_major=True, columns=0, even_columns=True, even_rows=False, align=True)
+        flow.column().operator("shirakumo_trial.rebake", text="ReBake Selected")
+        flow.column().operator("shirakumo_trial.rebake_all", text="ReBake All")
+        flow.column().operator("shirakumo_trial.rebake_and_export", text="ReBake & Export")
+
+registered_classes = [
+    ReBake,
+    ReBakeAll,
+    ReBakeAndExport,
+    SHIRAKUMO_PT_bake_panel,
+]
 
 def register():
-    pass
+    for cls in registered_classes:
+        bpy.utils.register_class(cls)
 
 def unregister():
-    pass
+    for cls in registered_classes:
+        bpy.utils.unregister_class(cls)
